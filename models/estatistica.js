ea3750703b98c0455d5b91757132a490a19bc89b
João <40210416@esmad.ipp.pt>
2025-05-21 12:01:51 +0100

    Added routes and another model to the project

---
+++ models/estatistica.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const estatisticaSchema = new mongoose.Schema({
+    tipo_estatistica: {
+        type: String,
+        enum: ['diaria', 'mensal', 'anual', 'semanal', 'prato'],
+        required: true
+    },
+    observacao: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: Date,
+        default: Date.now
+    },
+    dados: {
+        type: Object,
+        required: true
+    },
+})
+
+module.exports = mongoose.model('Estatistica', estatisticaSchema)
---
+++ routes/estatistica.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+
+const { validationResult, body, param } = require('express-validator')
